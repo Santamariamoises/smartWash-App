841e0fa5d25ff2b7e014517b5065a3ef860c86f4
jazzcelaya <jazzcelaya@gmail.com>
2018-10-09 18:55:02 -0500

    database merge

--- database/data.js
+++ database/data.js
@@ -7,18 +7,11 @@ const connection = mysql.createConnection({
   database  : 'smartWash'
 });
 
-<<<<<<< 34be37ddf3f64c355fb4b72e6ab670e4f4fcdb9a
-const insertHour = function(times, cb) {
-  connection.query(
-    "INSERT INTO deliver (times) VALUES (?)",
-    [times],
-=======
 const insertUser = function(mail, userName, callback) {
   console.log("quiubo desde la db");
   connection.query(
     'INSERT INTO users (mail, userName) VALUES (?, ?)',
     [mail, userName],
->>>>>>> fixed export errors
     (err, results, fields) => {
       if (err) {
         cb(err, null);
@@ -30,9 +23,4 @@ const insertUser = function(mail, userName, callback) {
   );
 };
 
-<<<<<<< 34be37ddf3f64c355fb4b72e6ab670e4f4fcdb9a
-module.exports.insertHour = insertHour;
-=======
-
 module.exports.insertUser = insertUser;
->>>>>>> fixed export errors
