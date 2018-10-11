78a181f59944662bbc7d2f50fdd6e21c524e0a9b
Angeloen <angeloen3000@gmail.com>
2018-10-11 18:33:34 -0500

    working material ui

--- client/src/userHome.jsx
+++ client/src/userHome.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import fire from './components/fire.jsx';
 import Status from './components/status.jsx'
 import { NavLink } from 'react-router-dom';
-
+import Button from '@material-ui/core/Button';
 
 class Home extends Component {
     constructor(props) {
@@ -29,7 +29,7 @@ class Home extends Component {
           <div>
             <img src={"/clothes.png"} />
             <p> logged to {this.props.user.email}
-            <button onClick={this.logout}>Logout</button>
+            <Button onClick={this.logout}>Logout</Button>
             </p>
             <button><NavLink to="/cita">Ordena ahora</NavLink></button>
             <h2> My laundry status </h2>
