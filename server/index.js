41e3c12a6c62451bbe12352eb0211d9bff4f8bf9
jazzcelaya <jazzcelaya@gmail.com>
2018-10-20 18:53:06 -0500

    rendering ussers info from db

--- server/index.js
+++ server/index.js
@@ -3,10 +3,8 @@ var bodyParser = require('body-parser');
 var app = express();
 var path = require('path');
 var url = require('url');
-//var route = require("./Routes.js");
 var db = require('../database/data.js');
 var stripe = require("stripe")("pk_test_wd9rThkNdTfjOnS9RXQIFPv6");
-
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/../client/dist'));
 app.use(bodyParser.urlencoded({extended: true}));
@@ -16,7 +14,6 @@ app.post('/users', function(req, res){
   let email = req.body.email;
   let userName = req.body.userName;
   console.log("hola desdel server");
-
   if(!email) {
     res.sendStatus(400);
   } else {
@@ -32,11 +29,10 @@ app.post('/users', function(req, res){
 
 app.post('/times', function(req, res){
   let times = req.body.times;
-
   if(!times) {
     res.sendStatus(400);
   } else {
-    database.insertTime (times, (err, results) => {
+    db.insertTime (times, (err, results) => {
       if (err) {
         res.status(500);
       } else {
@@ -53,11 +49,10 @@ app.post('/order', function(req, res){
   let size = req.body.size;
   let specialInd = req.body.specialInd;
   let service = req.body.service;
-
   if(!name) {
     res.sendStatus(400);
   } else {
-    database.insertOrder (name, phone, address, size, specialInd, service, (err, results) => {
+    db.insertOrder (name, phone, address, size, specialInd, service, (err, results) => {
       if (err) {
         res.status(500);
       } else {
@@ -69,7 +64,6 @@ app.post('/order', function(req, res){
 
 app.post('/api/stripe', function(req, res, next) {
   const stripeToken = req.body.stripeToken;
-
     stripe.charges.create({
   amount: 999,
   currency: 'usd',
@@ -90,8 +84,6 @@ app.post('/api/stripe', function(req, res, next) {
     }
   });
 });
-
-//app.get("user/:email",route.getResponse);
 
 app.get('/users', function (req, res) {
   db.selectUsers(function(err, data) {
@@ -114,18 +106,6 @@ app.get('/orders', function (req, res) {
     }
   });
 });
-
-//app.get('/user', function (req, res) {
-  // users.selectUser(email, function(err, data) {
-  //   if(err) {
-  //     console.log(err);
-  //     res.sendStatus(500);
-  //   } else {
-  //     console.log("get user request performed")
-  //     res.json(data);
-  //   }
-  // });
-// });
 
 app.listen(3000, function() {
   console.log('Server started and listening on port 3000');
