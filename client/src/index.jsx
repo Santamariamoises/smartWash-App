3dc1acffed1cc35cc5aa18e0cb7a07549204b1b7
JC <jazzcelaya@gmail.com>
2018-10-23 02:30:54 -0500

    form component refactored as stateless, working

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -26,10 +26,19 @@ class App extends React.Component {
       user: null,
       account: undefined,
       userOrders: undefined,
+      phone: '',
+      address: '',
+      size: '1-3 kg',
+      specialInd: '',
+      service: 'Laundry',
+      time: '',
     }
     this.getUserInfo = this.getUserInfo.bind(this);
     this.getUsersOrders = this.getUsersOrders.bind(this);
     this.authListener = this.authListener.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.addOrder = this.addOrder.bind(this);
+    //this.add = this.add.bind(this);
   }
 
       getUserInfo() {
@@ -56,6 +65,33 @@ class App extends React.Component {
          console.log('la cagaste desde el fronts',err)
        }
      })
+  }
+
+  handleChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+
+
+  addOrder(name, phone, address, size, specialInd, service){
+   $.ajax({
+     type: "POST",
+     url: "/order",
+     contentType: 'application/json',
+     data: JSON.stringify({
+       name: name,
+       phone: phone,
+       address: address,
+       size: size,
+       specialInd: specialInd,
+       service: service
+     }),
+     success:(data)=> {
+     },
+     error: (xhr,status,error) => {
+       console.log(error);
+     }
+   });
   }
 
   getUsersOrders() {
@@ -94,6 +130,23 @@ class App extends React.Component {
     });
   }
 
+//this function resets the form component back to default values but
+//i dont think well use it anymore
+  // addOrder(event) {
+  //     event.preventDefault();
+  //     this.addOrder(this.state.name, this.state.phone, this.state.address, this.state.size, this.state.specialInd, this.state.service);
+  //     this.setState({
+  //       phone: '',
+  //       address: '',
+  //       size: '',
+  //       specialInd: '',
+  //       service: ''
+  //     })
+  //   }
+
+
+
+
   componentDidMount() {
       navigator.geolocation.getCurrentPosition(location => {
         this.setState({
@@ -112,9 +165,13 @@ class App extends React.Component {
         <Navigation />
           <Switch>
             <Route exact path="/" component={About} />
+
             <Route path="/registro" render={(props) =>
               <Auth{...props} state={this.state} authListener={this.authListener}/>} />
-            <Route path="/Form" component={Form} />
+
+            <Route path='/Form' render={(props) =>
+            <Form {...props} state={this.state}  handleChange={this.handleChange}/>} />
+
             <Route path="/pickDay" component={Calendar} />
             <Route path='/micuenta' render={(props) =>
             <Home {...props} state={this.state}  getUsersOrders={this.getUsersOrders}
